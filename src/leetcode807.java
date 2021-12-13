dbf027c3c7f2faf3fa7dab6e42350587a05a48ea
Patrickaka <lijinglin361@gmail.com>
2021-12-14 00:37:36 +0800

    2021-12-14
    2021-12-14

--- src/leetcode807.java
+++ src/leetcode807.java
@@ -1,2 +1,26 @@
-public class leetcode807 {
+class Solution {
+    public static int maxIncreaseKeepingSkyline(int[][] grid) {
+        int length = grid.length;
+        int ans = 0;
+        int[] line1 = new int[length];
+        int[] line2 = new int[length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                line1[i] = Math.max(grid[i][j], line1[i]);
+                line2[j] = Math.max(grid[i][j], line2[j]);
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                ans += Math.min(line1[i], line2[j]) - grid[i][j];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}};
+        int ans = maxIncreaseKeepingSkyline(grid);
+        System.out.println(ans);
+    }
 }
