11452617bff3c4368b28b4947bc90623f4e88edc
Patrickaka <lijinglin361@gmail.com>
2021-12-14 21:20:05 +0800

    2021-12-14

--- src/leetcode630.java
+++ src/leetcode630.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 class Solution {
     public static int scheduleCourse(int[][] courses) {
-        int result = 0;
-
-        return result;
+        Arrays.sort(courses, Comparator.comparingInt(a -> a[1]));
+        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+        int sum = 0;
+        for (int[] c : courses) {
+            int d = c[0], e = c[1];
+            sum += d;
+            q.add(d);
+            if (sum > e) {
+                sum -= q.poll();
+            }
+        }
+        return q.size();
     }
 
     public static void main(String[] args) {
