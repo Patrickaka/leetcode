9619af67b549ce0adba6660bfa2347763be9accc
Patrickaka <lijinglin361@gmail.com>
2021-12-19 00:54:16 +0800

    2021-12-19

---
+++ src/leetcode997.java
@@ -0,0 +1,27 @@
+class Solution997 {
+
+    public static int findJudge(int n, int[][] trust) {
+        int ans = -1;
+        int[] he = new int[n + 1], cnt = new int[n + 1];
+        for (int[] trusts : trust) {
+            he[trusts[0]]++;
+            cnt[trusts[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (he[i] == 0 && cnt[i] == n - 1) {
+                ans = i;
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        int[][] trust = {{1, 2}};
+        int ans = findJudge(n, trust);
+        System.out.println(ans);
+    }
+}
