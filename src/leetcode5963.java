e5346bafee97be4f514aee74f0e410d2d95808e6
Patrickaka <lijinglin361@gmail.com>
2021-12-27 00:38:26 +0800

    2021-12-25

---
+++ src/leetcode5963.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution5963 {
+    public long[] getDistances(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        int n = arr.length;
+        long[] ans = new long[n];
+        Arrays.fill(ans, 0);
+        for (int i = 0; i < n; i++) {
+            List<Integer> temp = map.getOrDefault(arr[i], new ArrayList<>());
+            if (temp.size() != 0) {
+                int finalI = i;
+                temp.forEach(num -> {
+                    ans[finalI] += Math.abs(finalI - num);
+                    ans[num] += Math.abs(finalI - num);
+                });
+            }
+            temp.add(i);
+            map.put(arr[i], temp);
+        }
+        return ans;
+    }
+}
