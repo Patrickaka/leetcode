da99a3e82b6292741af75ff375a187fbb7f1869e
Patrickaka <lijinglin361@gmail.com>
2021-12-31 00:56:11 +0800

    2021-12-31

---
+++ src/leetcode507.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        int max = (int) Math.sqrt(num);
+        int ans = 1;
+        for (int i = 2; i <= max; i++) {
+            if (num % i == 0) {
+                int temp = i + num / i;
+                ans += temp;
+            }
+        }
+        return ans == num;
+    }
+}
