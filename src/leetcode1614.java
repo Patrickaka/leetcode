5f97936452482377cacc19401c10d063794870f0
lijinglin <lijinglin361@gmail.com>
2022-01-07 11:09:46 +0800

    2022-1-7

---
+++ src/leetcode1614.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDepth(String s) {
+        char[] chars = s.toCharArray();
+        int max = 0, temp = 0;
+        for (char c : chars) {
+            if (c == '(') {
+                temp++;
+                max = Math.max(max, temp);
+            } else if (c == ')') {
+                temp--;
+            }
+        }
+        return max;
+    }
+}
