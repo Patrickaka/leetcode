43d424ca8b7026791dc4175fc66e815120055931
Patrickaka <lijinglin361@gmail.com>
2022-06-08 23:14:42 +0800

    2022-6-8

---
+++ src/leetcode209.java
@@ -0,0 +1,25 @@
+class Solution209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length, ans = n + 10;
+        int[] sums = new int[n + 10];
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            int s = sums[i], d = s - target;
+            int l = 0, r = i;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (sums[mid] <= d) {
+                    l = mid;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            if (sums[r] <= d) {
+                ans = Math.min(ans, i - r);
+            }
+        }
+        return ans == n + 10 ? 0 : ans;
+    }
+}
---
+++ src/leetcode560.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution560 {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length, ans = 0;
+        int[] sums = new int[n + 10];
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 1; i <= n; i++) {
+            int s = sums[i], d = s - k;
+            ans += map.getOrDefault(d, 0);
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        return ans;
+    }
+}
