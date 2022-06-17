0b084b84bd009500e675fd7b6d388b2345762578
Patrickaka <lijinglin361@gmail.com>
2022-06-17 22:21:26 +0800

    2022-6-17

---
+++ src/leetcode798.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution798 {
+
+    static int N = 100010;
+    static int[] c = new int[N];
+
+    void add(int l, int r) {
+        c[l] += 1;
+        c[r + 1] -= 1;
+    }
+
+    public int bestRotation(int[] nums) {
+        Arrays.fill(c, 0);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int a = (i - (n - 1) + n) % n, b = (i - nums[i] + n) % n;
+            if (a <= b) {
+                add(a, b);
+            } else {
+                add(a, n - 1);
+                add(0, b);
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            c[i] += c[i - 1];
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (c[i] > c[ans]) {
+                ans = i;
+            }
+        }
+        return ans;
+    }
+}
