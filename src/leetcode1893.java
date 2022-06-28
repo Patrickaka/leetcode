b74eaf7b456448601fb5dca580e9c42f37122ecc
lijinglin <lijinglin361@gmail.com>
2022-06-28 11:44:18 +0800

    2022-6-28

---
+++ src/leetcode1893.java
@@ -0,0 +1,40 @@
+class Solution1893 {
+
+    int n = 55;
+    int[] tr = new int[n];
+
+    int lowbit(int x) {
+        return x & -x;
+    }
+
+    void add(int x, int v) {
+        for (int i = x; i <= n; i += lowbit(i)) {
+            tr[i] += v;
+        }
+    }
+
+    int query(int x) {
+        int ans = 0;
+        for (int i = x; i > 0; i -= lowbit(i)) {
+            ans += tr[i];
+        }
+        return ans;
+    }
+
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        for (int[] range : ranges) {
+            int a = range[0];
+            int b = range[1];
+            for (int j = a; j <= b; j++) {
+                add(j, 1);
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            if (query(i) - query(i - 1) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
