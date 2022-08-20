38ffe735942eb3f086b915945317b6a2b2ad57fd
Patrickaka <lijinglin361@gmail.com>
2022-08-21 01:32:24 +0800

    2022-8-21

--- src/offer/offer22/Solution.java
+++ src/offer/offer22/Solution.java
@@ -1,13 +1,24 @@
 package offer.offer22;
 
 
+/**
+ * 链表中倒数第k个节点
+ */
 class Solution {
+
+    /**
+     * 快慢指针
+     *
+     * @param head 输入一个链表
+     * @param k    倒数第k个节点
+     * @return 倒数第k个节点
+     */
     public ListNode getKthFromEnd(ListNode head, int k) {
         ListNode node = head;
-        for (int i = 0; i < k - 1; i++) {
+        for (int i = 0; i < k; i++) {
             node = node.next;
         }
-        while (node.next != null) {
+        while (node != null) {
             node = node.next;
             head = head.next;
         }
