70499d3654ed1953a8afae5ba0045f11bf5cd5c0
Patrickaka <lijinglin361@gmail.com>
2022-08-29 00:32:03 +0800

    2022-8-29

---
+++ src/meiri/leetcode1470/Solution.java
@@ -0,0 +1,14 @@
+package meiri.leetcode1470;
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[n * 2];
+        int l = 0, r = n;
+        int idx = 0;
+        while (l < n) {
+            ans[idx++] = nums[l++];
+            ans[idx++] = nums[r++];
+        }
+        return ans;
+    }
+}
--- src/zhousai/leetcode6161/Solution.java
+++ src/zhousai/leetcode6161/Solution.java
@@ -8,8 +8,6 @@ class Solution {
 
     public String removeStars(String s) {
         StringBuilder sb = new StringBuilder();
-        int prev = 0, prev1 = 0;
-        int sum = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '*') {
                 sb.delete(sb.length() - 1, sb.length());
