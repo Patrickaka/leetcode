a89d422b08be77d9ce99b51683806fdfc0f2ac1a
lijinglin <lijl@dianzhong.com>
2024-01-31 10:03:57 +0800

    update

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-S
-#src/tulun/leetcode778/Solution.java,e\c\ecbf74b910b987508eb5f4bacb9068e8b2205df8
-S
-#src/offerII/offerII03/Solution.java,3\9\39c4906a96c8221a7ef14733f589c87fe67e3904
-S
-#src/moni/leetcode1161/Solution.java,7\1\712ab5f838b0425a7986b8776dccec4be2803bde
---
+++ src/meiri/leetcode/editor/cn/FindTheMaximumNumberOfElementsInSubset.java
@@ -0,0 +1,93 @@
+//给你一个 正整数 数组 nums 。 
+//
+// 你需要从数组中选出一个满足下述条件的子集： 
+//
+// 
+// 你可以将选中的元素放置在一个下标从 0 开始的数组中，并使其遵循以下模式：[x, x², x⁴, ..., xᵏ/², xᵏ, xᵏ/², ..., x⁴
+//, x², x]（注意，k 可以是任何 非负 的 2 的幂）。例如，[2, 4, 16, 4, 2] 和 [3, 9, 3] 都符合这一模式，而 [2, 4, 
+//8, 4, 2] 则不符合。 
+// 
+//
+// 返回满足这些条件的子集中，元素数量的 最大值 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [5,4,1,2,2]
+//输出：3
+//解释：选择子集 {4,2,2} ，将其放在数组 [2,4,2] 中，它遵循该模式，且 2² == 4 。因此答案是 3 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,3,2,4]
+//输出：1
+//解释：选择子集 {1}，将其放在数组 [1] 中，它遵循该模式。因此答案是 1 。注意我们也可以选择子集 {2} 、{4} 或 {3} ，可能存在多个子集都
+//能得到相同的答案。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 10⁵ 
+// 1 <= nums[i] <= 10⁹ 
+// 
+//
+// Related Topics 数组 哈希表 枚举 👍 6 👎 0
+
+package meiri.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class FindTheMaximumNumberOfElementsInSubset {
+    public static void main(String[] args) {
+        Solution solution = new FindTheMaximumNumberOfElementsInSubset().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maximumLength(int[] nums) {
+            int res = 0;
+            Set<Integer> twoSet = new HashSet<>();
+            int sum = 1;
+            for (int i = 0; i < nums.length / 2; i++) {
+                twoSet.add(sum);
+                sum *= 2;
+            }
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            for (int start : map.keySet()) {
+                int cur = start;
+                int cnt = 0;
+                while (map.containsKey(cur)) {
+                    if (cur == 1) {
+                        cnt = 1;
+                        break;
+                    }
+                    cnt++;
+                    if (map.get(cur) < 2) {
+                        break;
+                    }
+                    cur *= start;
+                }
+                System.out.println("cnt:" + cnt + " " + "start:" + start);
+                if (twoSet.contains(cnt)) {
+                    res = Math.max(res, cnt * 2 - 1);
+                }
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
